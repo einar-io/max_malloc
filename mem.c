6312389b53a0239f336651f6b038c4096784f44b
Einar Rasmussen <einar@einar.io>
2024-10-01 18:20:41 +0200

    fmt

--- mem.c
+++ mem.c
@@ -1,17 +1,19 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 int main() {
-  size_t l=1, r=SIZE_MAX, m;
+  size_t l = 1, r = SIZE_MAX, m;
   void *p;
-  while (l<r-1){
-    m=l+((r-l)>>1);
-    printf("Allocating: %zu\n",m);
-    if  ( (p = malloc(m)) != NULL) l = m;
-    else r = m;
-    printf("Allocating: %zu %s\n",m, p!=NULL?"success":"failure");
-    p!=NULL?free(p):NULL;
+  while (l < r - 1) {
+    m = l + ((r - l) >> 1);
+    printf("Allocating: %zu\n", m);
+    if ((p = malloc(m)) != NULL)
+      l = m;
+    else
+      r = m;
+    printf("Allocating: %zu %s\n", m, p != NULL ? "success" : "failure");
+    p != NULL ? free(p) : NULL;
   }
   printf("SIZE_MAX: %zu\n", SIZE_MAX);
   printf("Largest possible allocation was %zu.\n", l);
