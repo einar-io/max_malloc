55b2328c783be59277383a7056ad85eef9b0b300
Einar Rasmussen <einar@einar.io>
2024-10-01 21:54:37 +0200

    add sysinfo

--- mem.c
+++ mem.c
@@ -1,6 +1,8 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/sysinfo.h>
 
 int main() {
   size_t l = 1, r = SIZE_MAX, m;
@@ -15,7 +17,20 @@ int main() {
     printf("Allocating: %zu %s\n", m, p != NULL ? "success" : "failure");
     p != NULL ? free(p) : NULL;
   }
-  printf("SIZE_MAX: %zu\n", SIZE_MAX);
-  printf("Largest possible allocation was %zu.\n", l);
+
+  struct sysinfo si;
+  assert(sysinfo(&si) == 0);
+  long sum = si.totalswap + si.totalram;
+  long diff = (long)sum - (long)l;
+
+  printf("\nSUMMARY (bytes)\n");
+  printf("--------------------------------------------------\n");
+  printf("SIZE_MAX:         %32.zu\n", SIZE_MAX);
+  printf("Total RAM:        %32.li\n", si.totalram);
+  printf("Total Swap:       %32.li\n", si.totalswap);
+  printf("RAM + Swap:       %32.li\n", sum);
+  printf("Max allocation:   %32.zu\n", l);
+  printf("Diff:             %+32.li\n", diff);
+
   return 0;
 }
